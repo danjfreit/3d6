5a62cad654a44c5acabdfe9d7e0837f147c17de0
Daniel Freitas <dan.j.freit@gmail.com>
2019-02-15 11:41:16 -0600

    Add basic Dice classDice class is static, based off of a Singleton ScriptableObject instanceRoll method can be called from anywhere

---
+++ 3D6/Assets/Scripts/Dice.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dice", menuName = "Dice", order = 0)]
+public class Dice : ScriptableObject {
+ 
+    private const string assetpath = "Dice";
+ 
+    private static Dice _instance;
+    private static Dice instance {
+        get { return _instance ?? (_instance = Resources.Load<Dice>(assetpath)); }
+    }
+
+    public GameObject die;
+    private static GameObject Die { get {return instance.die;} }
+
+    [MenuItem("PlayModeDebug/Roll")]
+    public static GameObject Roll() {
+        return GameObject.Instantiate(Die, new Vector3(0, 0, 5), Quaternion.identity);
+    }
+}
